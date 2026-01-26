765264a0913d533fecc547b35072e690452f40a2
Raghu-Ram-Reddy-Katkuri <raghuramreddyka7@gmail.com>
2026-01-26 12:09:13 +0530

    corrected organizer controller

--- backend/src/controllers/organizer.controller.js
+++ backend/src/controllers/organizer.controller.js
@@ -5,50 +5,50 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
 import { sendOrganizerApplicationMail } from "../utils/sendOrganizerApplicationMail.js";
 
- 
-const upgradeToOrganizer = asyncHandler(async (req, res) => {
-  const userId = req.user._id; // from auth middleware
+
+ const upgradeToOrganizer = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
   const { organizationName, phone } = req.body;
 
-   if (!organizationName || !phone ) {
-    throw new ApiError(400, " All fields are required");
+  if (!organizationName || !phone) {
+    throw new ApiError(400, "All fields are required");
   }
 
-   const user = await User.findById(userId);
+  const user = await User.findById(userId);
   if (!user) {
     throw new ApiError(404, "User not found");
   }
 
-   if (user.role === "ORGANIZER") {
-    throw new ApiError(400, "User is already an organizer");
+  const existingOrganizer = await Organizer.findOne({ userId });
+  if (existingOrganizer) {
+    throw new ApiError(400, "Organizer application already exists");
   }
 
-   const organizer = await Organizer.create({
-    userId: user._id,
+  const organizer = await Organizer.create({
+    userId,
     organizationName,
-    phone
- 
+    phone,
+    isVerified: false,
   });
 
-   user.role = "ORGANIZER";
-  await user.save();
-
+  // mail is async, non-blocking
   sendOrganizerApplicationMail(
-    user.email,          
-    user,                
-    organizationName   
+    user.email,
+    user,
+    organizationName
   ).catch((err) => {
     console.error("Email failed:", err.message);
   });
 
-   return res
-   .status(201)
-   .json
-   (
-    new ApiResponse(201,organizer, "Upgrade application successully submitted", {
-    })
-   );
+  return res.status(201).json(
+    new ApiResponse(
+      201,
+      organizer,
+      "Organizer application submitted successfully"
+    )
+  );
 });
+
 
 
 export { upgradeToOrganizer }
