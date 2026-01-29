fd16b8ba5464ca92080fd6306b0fa0034ef42cbd
Raghu-Ram-Reddy-Katkuri <raghuramreddyka7@gmail.com>
2026-01-30 00:11:10 +0530

    Revert "add publish and unpublish event functionality"

--- backend/src/routes/event.routes.js
+++ backend/src/routes/event.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
-import { createEvent, deleteEvent, updateEvent, getEventById, listOrganizerEvents, publishEvent, unPublishEvent ,  getTickets  } from "../controllers/event.controller.js";
-
+import { createEvent, deleteEvent, updateEvent, getEventById, listOrganizerEvents, getTickets } from "../controllers/event.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { requireOrganizer } from "../middlewares/role.middlewares.js";
 
@@ -27,12 +26,6 @@ router
 .get(verifyJWT,requireOrganizer,listOrganizerEvents)
 
 router
-.route("/publishevent/:eventId")
-.patch(verifyJWT,requireOrganizer,publishEvent)
-
-router
-.route("/unpublishevent/:eventId")
-.patch(verifyJWT,requireOrganizer,unPublishEvent)
 .route("/tickets/:eventId")
 .get(verifyJWT,requireOrganizer,getTickets)
 
