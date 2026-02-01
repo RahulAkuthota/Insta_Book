a68fbc2bae6ff206c946d3419d69ad02b068b780
Rahul Akuthota <rahulakuthota5@gmail.com>
2026-02-01 13:34:07 +0530

    Add myBookings

--- backend/src/routes/booking.routes.js
+++ backend/src/routes/booking.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createFreeBooking } from "../controllers/booking.controller.js";
+import { createFreeBooking, myBookings } from "../controllers/booking.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/free/:eventId/:ticketId").post(verifyJWT, createFreeBooking);
+
+router.route("/mybookings").get(verifyJWT, myBookings);
+
 
 export default router;
